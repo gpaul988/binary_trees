fd59f469b94cda8944fa59ce631cb38278dd7af8
gpaul988 <gpaul988@gmail.com>
2023-08-30 11:51:48 -0700

    4-binary_tree_is_leaf complete

--- 4-binary_tree_is_leaf.c
+++ 4-binary_tree_is_leaf.c
@@ -1,12 +1,11 @@
 #include "binary_trees.h"
 
-/* Graham S. Paul (4-binary_tree_is_leaf.c) */
-
 /**
  * binary_tree_is_leaf - Examines if node is leaf of a binary tree.
  * @node: Directs to  node to examine.
  * Return: When node is leaf - 1.
  *         O/W - 0.
+ * Author: Graham S. Paul (4-binary_tree_is_leaf.c)
  */
 
 int binary_tree_is_leaf(const binary_tree_t *node)

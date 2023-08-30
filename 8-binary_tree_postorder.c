2ac75d92c5cf2643d3a60572ae0b28d7b028f988
gpaul988 <gpaul988@gmail.com>
2023-08-30 12:38:08 -0700

    fixes

--- 8-binary_tree_postorder.c
+++ 8-binary_tree_postorder.c
@@ -7,7 +7,6 @@
  * @func: Direct to purpose to call each node.
  * Author: Graham S. Paul (8-binary_tree_postorder.c)
  */
-
 void binary_tree_postorder(const binary_tree_t *tree, void (*func)(int))
 {
 	if (tree && func)

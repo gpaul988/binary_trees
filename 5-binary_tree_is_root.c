1b8dc5084f6f13c250fe98e9e76658ce4ab5b815
gpaul988 <gpaul988@gmail.com>
2023-08-30 11:53:43 -0700

    -binary_tree_is root fixes

--- 5-binary_tree_is_root.c
+++ 5-binary_tree_is_root.c
@@ -1,12 +1,12 @@
 #include "binary_trees.h"
 
-/* Graham S. Paul (5-binary_tree_is root.c) */
 
 /**
  * binary_tree_is_root - Examines if  node is home of a binary tree.
  * @node: Directs to node to Examine.
  * Return: When the node is a root - 1.
  *         O/W - 0.
+ * Author: Graham S. Paul (5-binary_tree_is root.c)
  */
 
 int binary_tree_is_root(const binary_tree_t *node)

e6bd7bf3189337dc8ece07a85b32d610f16cb01c
gpaul988 <gpaul988@gmail.com>
2023-08-30 11:47:20 -0700

    3-binary_tree_delete minor correction

--- 3-binary_tree_delete.c
+++ 3-binary_tree_delete.c
@@ -1,11 +1,11 @@
 #include "binary_trees.h"
 
-/* Graham S. Paul (3-binary_tree_delete.c) */
 
 /**
  * binary_tree_delete - Removes binary tree.
  * @tree: Directs to node of the tree to remove.
  * Return: void
+ * Author: Graham S. Paul (3-binary_tree_delete.c)
  */
 
 void binary_tree_delete(binary_tree_t *tree)

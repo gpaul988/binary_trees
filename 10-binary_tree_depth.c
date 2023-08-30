f96329d888f2f95a82bc014d78be7fae84c33c42
gpaul988 <gpaul988@gmail.com>
2023-08-30 12:41:57 -0700

    fixes

--- 10-binary_tree_depth.c
+++ 10-binary_tree_depth.c
@@ -7,7 +7,6 @@
  * Return: 0 NULL, O/W depth
  * Author: Graham S. Paul (10-binary_tree_depth.c)
  */
-
 size_t binary_tree_depth(const binary_tree_t *tree)
 {
 	return ((tree && tree->parent) ? 1 + binary_tree_depth(tree->parent) : 0);
